566c187f2b32a94fdea7e9906bd6d33d69625a77
dlehdrjs36 <41173462+dlehdrjs36@users.noreply.github.com>
2022-02-23 17:55:23 +0900

    [프로젝트 설정] SSH 설정 값 조회 수정 (#5)

--- src/main/java/com/projectteam/coop/util/SSHConnection.java
+++ src/main/java/com/projectteam/coop/util/SSHConnection.java
@@ -4,23 +4,36 @@ import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
+import java.util.Properties;
 
 public class SSHConnection {
 
-    private final static String S_PATH_FILE_PRIVATE_KEY = "";
-    private final static String S_PATH_FILE_KNOWN_HOSTS = "";
-    private final static String S_PASS_PHRASE = "";
-    private final static String BASTION_SERVER = "";
-    private final static int BASTION_PORT = 0; // 해당 서버에서 사용할 수 있는 포트 아무거나 입력
-    private final static String SSH_USER = "";
-    private final static String SSH_REMOTE_SERVER = "";
-    private final static int SSH_REMOTE_PORT = 22;
-    private final static String MYSQL_REMOTE_SERVER = "";
-    private final static int MYSQL_REMOTE_PORT = 0;
+    private final String S_PATH_FILE_PRIVATE_KEY;
+    private final String S_PATH_FILE_KNOWN_HOSTS;
+    private final String S_PASS_PHRASE;
+    private final String SSH_USER;
+    private final String SSH_REMOTE_SERVER;
+    private final int SSH_REMOTE_PORT;
+    private final String BASTION_SERVER;
+    private final int BASTION_PORT; // 해당 서버에서 사용할 수 있는 포트 아무거나 입력
+    private final String MYSQL_REMOTE_SERVER;
+    private final int MYSQL_REMOTE_PORT;
     private Session session; //represents each ssh session
     private Channel channel;
 
-    public SSHConnection() {
+    public SSHConnection(Properties properties) {
+        this.S_PATH_FILE_PRIVATE_KEY = properties.getProperty("ssh.privateKeyPath");
+        this.S_PATH_FILE_KNOWN_HOSTS = properties.getProperty("ssh.knownHostsPath");
+        this.S_PASS_PHRASE = properties.getProperty("ssh.privateKeyPassword");
+        this.SSH_USER = properties.getProperty("ssh.user");
+        this.SSH_REMOTE_SERVER = properties.getProperty("ssh.remoteServer");
+        this.SSH_REMOTE_PORT = Integer.parseInt(properties.getProperty("ssh.remotePort"));
+        //SSH tunnel
+        this.BASTION_SERVER = properties.getProperty("ssh.bastionServer");
+        this.BASTION_PORT = Integer.parseInt(properties.getProperty("ssh.bastionPort"));
+        this.MYSQL_REMOTE_SERVER = properties.getProperty("ssh.mysqlRemoteServer");
+        this.MYSQL_REMOTE_PORT = Integer.parseInt(properties.getProperty("ssh.mysqlRemotePort"));
+
         JSch jsch;
         jsch = new JSch();
         try {
--- src/main/java/com/projectteam/coop/util/SSHContextListener.java
+++ src/main/java/com/projectteam/coop/util/SSHContextListener.java
@@ -3,6 +3,10 @@ package com.projectteam.coop.util;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @WebListener
 public class SSHContextListener implements ServletContextListener {
@@ -16,10 +20,17 @@ public class SSHContextListener implements ServletContextListener {
     //앱이 시작되면 SSH 연결 생성.
     public void contextInitialized(ServletContextEvent arg0) {
         System.out.println("Context initialized ... !");
-        try {
-            conexionssh = new SSHConnection();
-        } catch (Throwable e) {
-            e.printStackTrace(); // error connecting SSH server
+        ClassLoader classLoader = getClass().getClassLoader();
+        Properties sshConfig = new Properties();
+
+        try (InputStream is =  classLoader.getResourceAsStream("props/system/dev.properties")) {
+            sshConfig.load(is);
+            conexionssh = new SSHConnection(sshConfig);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 

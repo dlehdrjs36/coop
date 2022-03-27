42e758159b24b55fca7b1ad625809d0d07a91eb3
dglee <slsldkf123@naver.com>
2022-03-27 23:57:45 +0900

    [프로젝트 설정] 프로젝트 구조 수정

--- src/main/java/com/projectteam/coop/jpasample/repository/JpaSampleRepository.java
+++ src/main/java/com/projectteam/coop/jpasample/repository/JpaSampleRepository.java
@@ -2,29 +2,50 @@ package com.projectteam.coop.jpasample.repository;
 
 import com.projectteam.coop.jpasample.domain.Member;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-@Transactional(transactionManager = "hibernateTxManager")
 public class JpaSampleRepository {
 
-    @PersistenceContext(unitName = "mysqlJpa")
-    EntityManager em;
+    @PersistenceContext(unitName = "h2Jpa")
+    private EntityManager em;
     //EntityManagerFactory emf = Persistence.createEntityManagerFactory("memoryJpa"); //스프링 없이 사용하려면 직접 팩토리 만들어서 생성필요
 
-    public Long join(Member member) {
+    public Long addMember(Member member) {
         em.persist(member);
         return member.getSampleSeq();
+    }
+
+    public Long deleteMember(Long id) {
+        Member findMember = em.find(Member.class, id);
+        if(findMember != null) {
+            em.remove(findMember);
+            return findMember.getSampleSeq();
+        }
+        throw new IllegalArgumentException("해당하는 회원이 존재하지 않습니다.");
+    }
+
+    public Long updateMember(Long id, Member member) {
+        Member findMember = em.find(Member.class, id);
+        findMember.setId(member.getId());
+        findMember.setName(member.getName());
+        findMember.setAge(member.getAge());
+        return findMember.getSampleSeq();
     }
 
     public Optional<Member> findById(Long id) {
         Member member = em.find(Member.class, id);
         return Optional.ofNullable(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
     }
 
     public void sample() {
@@ -55,12 +76,4 @@ public class JpaSampleRepository {
         }
     }
 
-    public void sample2() {
-        Member member = new Member();
-        member.setId("test"+member.getId());
-        member.setName("sample"+member.getId());
-        member.setAge("20"+member.getId());
-
-        em.persist(member);
-    }
 }
--- src/main/java/com/projectteam/coop/jpasample/service/JpaSampleService.java
+++ src/main/java/com/projectteam/coop/jpasample/service/JpaSampleService.java
@@ -2,11 +2,18 @@ package com.projectteam.coop.jpasample.service;
 
 import com.projectteam.coop.jpasample.domain.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JpaSampleService {
 
-    Long join(Member member);
+    Long addMember(Member member);
 
-    Optional<Member> findById(Long id);
+    Long deleteMember(Long id);
+
+    Long updateMember(Long id, Member member);
+
+    Optional<Member> findMember(Long id);
+
+    List<Member> findMembers();
 }

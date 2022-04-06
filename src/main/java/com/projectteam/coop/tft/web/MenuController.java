15509c52e48d4f810f0fcc3c54f90b77fa6e5935
tunta3586 <69376193+tunta3586@users.noreply.github.com>
2022-04-06 17:25:12 +0900

    챔피언 리스트 Mok화면 추가 (#18)
    * 챔피언 이미지, Mok에 사용한 헤더 부분 이미지 추가
    시너지 아이콘 추가
    
    출처
    챔피언 및 헤더 이미지
    https://developer.riotgames.com/docs/lol
    -> https://ddragon.leagueoflegends.com/cdn/dragontail-12.6.1.tgz
    
    실코 이미지
    LOL 공식 유투브
    https://www.youtube.com/watch?v=zXGIOnh44qs&t=77s
    
    시너지(공식)
    https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt72c1e880e8010a10/61f61fd6fb66f771578d7ed9/TFT_6.5_TraitOverview-CheatSheet_3000x1720_ALyu_Final_KR.png
    를 참조
    
    * <기존 추가 및 수정>
    bodyHeader.html
    ->네비바 챔피언 항목 경로지정
    
    headerNaviController.java
    -> MenuController.java 로 변경
    -> GetMapping방식으로 championList 페이지 경로 설정
    
    <추가>
    championList.html, championList.js, layoutChampionListPage.css
    -> 챔피언 리스트 html mok 추가, 관련 파일들

--- src/main/java/com/projectteam/coop/tft/web/MenuController.java
+++ src/main/java/com/projectteam/coop/tft/web/MenuController.java
@@ -4,13 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
-public class headerNaviController {
+public class MenuController {
 
     @GetMapping(value = "/augmentsList")
-    public String getAugmentationListPage(Model model) {
+    public String getAugmentsListPage(Model model) {
         return "/templates/tft/augmentsList";
+    }
+
+    @GetMapping(value = "/championList")
+    public String getChampionListPage(Model model) {
+        return "/templates/tft/championList";
     }
 }

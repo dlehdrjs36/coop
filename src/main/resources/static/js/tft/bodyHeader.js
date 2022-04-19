3d2a264f112f0027e709d3729014a8c327707777
tunta3586 <69376193+tunta3586@users.noreply.github.com>
2022-04-19 17:18:46 +0900

    [헤더 검색기능 추가] 챔피언 검색 기능 추가 (#38)
    챔피언 검색 기능 추가
    -> 챔피언 검색시 챔피언 리스트 화면으로 전환 입력한 챔피언 이름에 맞는게 있으면 해당 챔피언의 데이터를 초기화면으로 리스트 페이지에 표시
    -> 챔피언 검색시 한글 한정으로 챔피언 이름을 확인하여서 일부 같은 경우 완성된 챔피언명이 표시되게 추가함
    -> 해당 부분은 마우스 커서를 올려놓으면 색이 변하고 챔피언이름을 클릭할시 해당 이름은 챔피언 검색창에 입력되게 추가

---
+++ src/main/resources/static/js/tft/bodyHeader.js
@@ -0,0 +1,63 @@
+const championNameList = ["Ahri","Alistar","Ashe","Blitzcrank","Brand","Braum","Caitlyn","Camille","ChoGath","Corki","Darius","Draven","Ekko","Ezreal","Galio","Gangplank","Gnar","Illaoi","Irelia","JarvanIV","Jayce","Jhin","Jinx","Kaisa","Kassadin","KhaZix","Leona","Lucian","Lulu","Malzahar","MissFortune","Morgana","Nocturne","Orianna","Poppy","Quinn","RekSai","Renata","Sejuani","Senna","Seraphine","Silco","Singed","Sivir","Swain","Syndra","TahmKench","Talon","Tryndamere","Twitch","Veigar","Vex","Vi","Viktor","Warwick","Zac","Zeri","Ziggs","Zilean","Zyra"];
+const championNameKrList = ["아리","알리스타","애쉬","블리츠크랭크","브랜드","브라움","케이틀린","카밀","초가스","코르키","다리우스","드레이븐","에코","이즈리얼","갈리오","갱플랭크","나르","일라오이","이렐리아","자르반4세","제이스","진","징크스","카이사","카사딘","카직스","레오나","루시안","룰루","말자하","미스포츈","모르가나","녹턴","오리아나","뽀삐","퀸","렉사이","레나타글라스크","세주아니","세나","세라핀","실코","신지드","시비르","스웨인","신드라","탐켄치","탈론","트린다미어","트위치","베이가","벡스","바이","빅토르","워윅","자크","제리","직스","질리언","자이라"];
+const searchChampionName = document.getElementById("searchChampionName");
+const keywords = document.querySelector(".championKeywords");
+let keywordsFlg = false;
+function searchChampion(e){
+    let search = e.target.value;
+    let keywords = document.querySelector(".championKeywords");
+
+    if(keywords.innerHTML != "") {
+        while ( keywords.hasChildNodes() ) { keywords.removeChild( keywords.firstChild ); }
+    }
+
+    if(search === "") {
+        keywords.classList.add("hide");
+    } else {
+        keywords.classList.remove("hide");
+    }
+
+    if( search !== "") {
+        for(let i = 0; i < championNameKrList.length; i++){
+            if(championNameKrList[i].indexOf(search) !== -1){
+                const innerHtml = '<div class="championKeyword" name="' + championNameList[i] + '" onclick=insertSearchKeyword("' + i + '")>' + championNameKrList[i] + '</div>';
+                keywords.innerHTML += innerHtml;
+            }
+        }
+    }
+}
+
+function insertSearchKeyword( keyword ){
+    if(keyword.type !== "") {
+        searchChampionName.value = championNameKrList[keyword];
+    }
+    document.querySelector(".championKeywords").classList.add("hide");
+}
+
+function championSearch(){
+    let doc = document.searchChampion;
+    let searchChampionName = doc.searchChampionName.value;
+    for(let i=0; i<championNameKrList.length; i++){
+        if(searchChampionName === championNameKrList[i]){
+            doc.action = "/championList/" + championNameList[i];
+            doc.submit();
+            return "";
+        }
+    }
+    doc.action = "/championList";
+    doc.submit();
+}
+
+searchChampionName.addEventListener('input', searchChampion);
+searchChampionName.addEventListener('focus', searchChampion);
+keywords.addEventListener('mouseenter', function (){
+    keywordsFlg = true;
+});
+keywords.addEventListener('mouseleave', function (){
+    keywordsFlg = false;
+});
+searchChampionName.addEventListener('focusout', function (){
+    if(!keywordsFlg){
+        document.querySelector(".championKeywords").classList.add("hide");
+    }
+});

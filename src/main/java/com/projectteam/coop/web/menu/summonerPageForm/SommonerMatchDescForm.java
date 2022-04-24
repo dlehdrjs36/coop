58485c7a9c75b2f8e4d8c59b475463ba2f6315c8
lee <tunta3586@gmail.com>
2022-04-24 19:27:26 +0900

    [소환사 페이지 수정] 소환사 페이지 디자인 수정 경기 기록에서 소환사 닉네임 클릭할시 해당 소환사 검색 진행

---
+++ src/main/java/com/projectteam/coop/web/menu/summonerPageForm/SommonerMatchDescForm.java
@@ -0,0 +1,36 @@
+package com.projectteam.coop.web.menu.summonerPageForm;
+
+import com.projectteam.coop.tft.domain.MatchDesc;
+import com.projectteam.coop.util.TftUtil;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+public class SommonerMatchDescForm {
+        private String gameDatetime;
+        private MatchDesc matchDesc;
+        private List<Units> units;
+
+        public List<SommonerMatchDescForm> createSommonerMatchDescForm(List<MatchDesc> matchDesc){
+                List<SommonerMatchDescForm> sommonerMatchDescFormList = new ArrayList<>();
+                Units units = new Units();
+                TftUtil tftUtil = new TftUtil();
+                int i;
+                for(i=0; i<matchDesc.size(); i++) {
+                        SommonerMatchDescForm sommonerMatchDescForm = new SommonerMatchDescForm();
+                        sommonerMatchDescForm.gameDatetime = tftUtil.getGameDatetime(matchDesc.get(i).getGameDatetime());
+                        sommonerMatchDescForm.matchDesc = matchDesc.get(i);
+                        sommonerMatchDescFormList.add(sommonerMatchDescForm);
+                        sommonerMatchDescForm.units = units.createUnits(
+                                matchDesc.get(i).getUnitsCharacterId(),
+                                matchDesc.get(i).getUnitsItemNames(),
+                                matchDesc.get(i).getUnitsRarity(),
+                                matchDesc.get(i).getUnitsTier()
+                        );
+                }
+                return sommonerMatchDescFormList;
+        }
+}

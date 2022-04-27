05c51a4f8cf657a0b2971a588ba81ea1db5863bb
lee <tunta3586@gmail.com>
2022-04-27 13:21:49 +0900

    [화면 추가] 회원 정보 수정 페이지 추가 기능 -> 비밀번호 변경 기능 추가 변경버튼을 눌렀을시 -> 현재 비밀번호, 비밀번호, 비밀번호 확인에 입력이 없을시 화면 메세지 표시 submit발생하지 않음 -> 비밀번호, 비밀번호 확인 일치하지 않을시 화면 메세지 표시 submit발생하지 않음 -> 위의 조건이 충족했을시 submit 발생함 서버 컨트롤에서 입력한 현재 비밀번호를 이용해서 findMember를 구동 멤버가 없는경우 -> 회원수정 화면으로 재이동 세션에 회원 데이터 없는 경우  -> 아무런 변화가 없이 홈 화면으로 이동 위의 경우 이외의 경우  -> 수정된 데이터를 업데이트 한뒤 홈 화면으로 이동

--- src/main/java/com/projectteam/coop/web/member/MemberController.java
+++ src/main/java/com/projectteam/coop/web/member/MemberController.java
@@ -1,7 +1,10 @@
 package com.projectteam.coop.web.member;
 
 import com.projectteam.coop.domain.Member;
+import com.projectteam.coop.domain.Product;
 import com.projectteam.coop.service.member.MemberService;
+import com.projectteam.coop.web.argumentresolver.Login;
+import com.projectteam.coop.web.session.MemberSessionDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +14,9 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.http.HttpRequest;
 
 @Controller
 @RequiredArgsConstructor
@@ -45,6 +51,59 @@ public class MemberController {
         Member member = Member.createMember(memberForm.getEmail(), memberForm.getName(), memberForm.getPassword(), Boolean.TRUE);
         memberService.addMember(member);
 
+        return "redirect:/";
+    }
+
+    @GetMapping("/members/update")
+    public String editProfile(@Login MemberSessionDto loginMember, Model model) {
+
+        //세션에 회원 데이터 없는 경우
+        if (loginMember == null) {
+            return "redirect:/login";
+        }
+
+        Member member = memberService.findMember(loginMember.getId());
+        if (member != null) {
+            MemberForm memberForm = new MemberForm();
+            memberForm.setId(member.getId());
+            memberForm.setName(member.getName());
+            memberForm.setEmail(member.getEmail());
+            memberForm.setEmailReceptionType(member.getEmailReceptionType());
+
+
+            model.addAttribute("memberForm", memberForm);
+            return "/templates/members/updateMemberForm";
+        }
+        //회원 정보 없음
+        return "redirect:/";
+    }
+
+    @PostMapping("/members/update")
+    public String editPassword(@Login MemberSessionDto loginMember, HttpServletRequest httpRequest, Model model){
+        String currentPassword = httpRequest.getParameter("currentPassword");
+        String changePassword = httpRequest.getParameter("password");
+
+        //이메일과 패스워드가 일치하는 회원 검색
+        Member findMember = memberService.findMember(loginMember.getEmail(), currentPassword);
+        Member member = memberService.findMember(loginMember.getId());
+        MemberForm memberForm = new MemberForm();
+        memberForm.setId(member.getId());
+        memberForm.setName(member.getName());
+        memberForm.setEmail(member.getEmail());
+        memberForm.setEmailReceptionType(member.getEmailReceptionType());
+
+        if(findMember == null) {
+            if (loginMember != null) {
+                model.addAttribute("memberForm", memberForm);
+                model.addAttribute("errorMessage","입력한 현재 비밀번호가 일치하지 않습니다.");
+                return "/templates/members/updateMemberForm";
+            }
+            //회원 정보 없음
+            return "redirect:/";
+        }
+
+        memberForm.setPassword(changePassword);
+        memberService.updateMember(memberForm);
         return "redirect:/";
     }
 }

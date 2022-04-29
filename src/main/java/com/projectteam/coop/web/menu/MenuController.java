d19ee4710c67cddf3dee1dd510ba9c4b9c0a14eb
lee <tunta3586@gmail.com>
2022-04-29 17:29:22 +0900

    [챔피언 리스트 페이지 수정] 챔피언, 시너지 리스트 관리 기존 JS 정의에서 Json파일로 전환 일부 html 수정 챔피언 리스트 스크롤 수정 기존 수정된 시너지 이름, 챔피언 이름 -> 대응

--- src/main/java/com/projectteam/coop/web/menu/MenuController.java
+++ src/main/java/com/projectteam/coop/web/menu/MenuController.java
@@ -7,17 +7,23 @@ import com.projectteam.coop.tft.domain.Synergy;
 import com.projectteam.coop.tft.service.MatchDescService;
 import com.projectteam.coop.tft.service.SynergyService;
 import com.projectteam.coop.util.TftUtil;
-import com.projectteam.coop.web.menu.summonerPageForm.MatchDescForm;
 import com.projectteam.coop.web.menu.summonerPageForm.SommonerMatchDescForm;
 import com.projectteam.coop.web.menu.summonerPageForm.SummonerForm;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.*;
 import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -39,6 +45,32 @@ public class MenuController {
 
     @GetMapping(value = "/tft/championList")
     public String getChampionListPage(Model model) {
+        JSONParser parser = new JSONParser();
+        final ResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource resource = resourceLoader.getResource("classpath:/static/json/tft/");
+        // JSON 파일 읽기
+        JSONObject championListJsonObject = null;
+        JSONObject synergyListJsonObject = null;
+        try {
+            FileInputStream Input = new FileInputStream(resource.getURL().getPath()+"championList.json");
+            InputStreamReader reader = new InputStreamReader(Input, "UTF-8");
+            championListJsonObject = (JSONObject) parser.parse(reader);
+
+            Input = new FileInputStream(resource.getURL().getPath()+"synergyList.json");
+            reader = new InputStreamReader(Input, "UTF-8");
+            synergyListJsonObject = (JSONObject) parser.parse(reader);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        model.addAttribute("championListData", championListJsonObject);
+        model.addAttribute("synergyListData", synergyListJsonObject);
+
         return "/templates/tft/championList";
     }
 

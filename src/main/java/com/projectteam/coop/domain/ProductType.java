9f0b78f56ee1556e0f243c152e234341ebf1b2d6
dlehdrjs36 <41173462+dlehdrjs36@users.noreply.github.com>
2022-05-02 23:33:06 +0900

    [상품 기능] 상점 화면 디자인 수정 및 상품 타입 수정 (#73)

--- src/main/java/com/projectteam/coop/domain/ProductType.java
+++ src/main/java/com/projectteam/coop/domain/ProductType.java
@@ -1,5 +1,15 @@
 package com.projectteam.coop.domain;
 
 public enum ProductType {
-    BACKGROUND, ICON
+    BACKGROUND("배경"), ICON("아이콘");
+
+    private String description;
+
+    ProductType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }

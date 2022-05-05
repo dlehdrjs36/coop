245718a3606d4d6d6fb58186bc336a25d72d4db3
dglee <slsldkf123@naver.com>
2022-05-06 00:16:50 +0900

    [댓글 기능] API 예외 처리 추가

---
+++ src/main/java/com/projectteam/coop/web/exhandler/advice/ExControllerAdvice.java
@@ -0,0 +1,41 @@
+package com.projectteam.coop.web.exhandler.advice;
+
+import com.projectteam.coop.exception.CommentNotFoundException;
+import com.projectteam.coop.web.exhandler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@Slf4j
+public class ExControllerAdvice {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    public ResponseEntity<ErrorResult> commentExHandler(CommentNotFoundException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("COMMENT_NOT_FOUND", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResult exHandler(Exception e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("SERVER_ERROR", "내부 오류");
+    }
+
+}

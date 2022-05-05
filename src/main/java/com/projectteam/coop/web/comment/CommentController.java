421178eaf85f4544b06fb965d9dfa69cc37fe979
dglee <slsldkf123@naver.com>
2022-05-06 00:17:01 +0900

    [댓글 기능] API 예외 처리 추가

--- src/main/java/com/projectteam/coop/web/comment/CommentController.java
+++ src/main/java/com/projectteam/coop/web/comment/CommentController.java
@@ -1,15 +1,11 @@
 package com.projectteam.coop.web.comment;
 
-import com.projectteam.coop.exception.CommentNotFoundException;
 import com.projectteam.coop.service.comment.CommentService;
 import com.projectteam.coop.web.argumentresolver.Login;
-import com.projectteam.coop.web.exhandler.ErrorResult;
 import com.projectteam.coop.web.post.CommentCreateForm;
 import com.projectteam.coop.web.session.MemberSessionDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -26,30 +22,6 @@ import java.util.Optional;
 public class CommentController {
 
     private final CommentService commentService;
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResult illegalExHandler(IllegalArgumentException e) {
-        log.error("[exceptionHandler] ex", e);
-        return new ErrorResult("BAD", e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseBody
-    public ResponseEntity<ErrorResult> commentExHandler(CommentNotFoundException e) {
-        log.error("[exceptionHandler] ex", e);
-        ErrorResult errorResult = new ErrorResult("COMMENT_NOT_FOUND", e.getMessage());
-        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    @ResponseBody
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResult exHandler(Exception e) {
-        log.error("[exceptionHandler] ex", e);
-        return new ErrorResult("SERVER_ERROR", "내부 오류");
-    }
 
     @PostMapping("/{postId}")
     public String create(@Login MemberSessionDto memberSessionDto,

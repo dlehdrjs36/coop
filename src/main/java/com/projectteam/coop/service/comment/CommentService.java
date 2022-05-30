a40296f758ab5796b6f8665db7c8658896e6ac21
dglee <slsldkf123@naver.com>
2022-05-30 23:59:45 +0900

    [리팩토링] 댓글 기능 메서드 분리 및 명칭 수정

--- src/main/java/com/projectteam/coop/service/comment/CommentService.java
+++ src/main/java/com/projectteam/coop/service/comment/CommentService.java
@@ -11,7 +11,6 @@ import com.projectteam.coop.repository.post.PostRepository;
 import com.projectteam.coop.web.post.CommentCreateForm;
 import com.projectteam.coop.web.session.MemberSessionDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,37 +26,42 @@ public class CommentService {
     private final MemberRepository memberRepository;
 
 
+    //댓글 등록
     public Long addComment(CommentCreateForm commentCreateForm, MemberSessionDto loginMember) {
-        Comment comment;
         Post post = postRepository.findPost(commentCreateForm.getPostId());
-
-        if (loginMember == null) {
-            comment = Comment.createComment(post, commentCreateForm.getPassword(), commentCreateForm.getContent(), commentCreateForm.getNickname());
-        } else {
-            MemberSessionDto session = loginMember;
-            Member member = memberRepository.findMember(session.getId());
-            comment = Comment.createComment(post, commentCreateForm.getPassword(), commentCreateForm.getContent(), member);
-        }
+        Comment comment = getComment(commentCreateForm, loginMember, post);
         return commentRepository.addComment(comment);
     }
 
-    public Long addChildComment(CommentCreateForm commentCreateForm, MemberSessionDto loginMember) {
+    private Comment getComment(CommentCreateForm commentCreateForm, MemberSessionDto loginMember, Post post) {
         Comment comment;
-        Post post = postRepository.findPost(commentCreateForm.getPostId());
-
         if (loginMember == null) {
-            Comment upperComment = commentRepository.findComment(commentCreateForm.getUpperCommentId());
-            commentRepository.orderSort(upperComment.getId());
+            comment = Comment.createComment(post, commentCreateForm.getPassword(), commentCreateForm.getContent(), commentCreateForm.getNickname());
+        } else {
+            Member member = memberRepository.findMember(loginMember.getId());
+            comment = Comment.createComment(post, commentCreateForm.getPassword(), commentCreateForm.getContent(), member);
+        }
+        return comment;
+    }
+
+    //대댓글 등록
+    public Long addChildComment(CommentCreateForm commentCreateForm, MemberSessionDto loginMember) {
+        Post post = postRepository.findPost(commentCreateForm.getPostId());
+        Comment comment = getChildComment(commentCreateForm, loginMember, post);
+        return commentRepository.addComment(comment);
+    }
+
+    private Comment getChildComment(CommentCreateForm commentCreateForm, MemberSessionDto loginMember, Post post) {
+        Comment comment;
+        Comment upperComment = commentRepository.findComment(commentCreateForm.getUpperCommentId());
+        commentRepository.sortCommentsByOrder(upperComment.getId());
+        if (loginMember == null) {
             comment = Comment.createChildComment(post, commentCreateForm.getPassword(), commentCreateForm.getContent(), commentCreateForm.getNickname(), upperComment);
         } else {
-            MemberSessionDto session = loginMember;
-            Member member = memberRepository.findMember(session.getId());
-            Comment upperComment = commentRepository.findComment(commentCreateForm.getUpperCommentId());
-            commentRepository.orderSort(upperComment.getId());
+            Member member = memberRepository.findMember(loginMember.getId());
             comment = Comment.createChildComment(post, commentCreateForm.getPassword(), commentCreateForm.getContent(), member, upperComment);
         }
-
-        return commentRepository.addComment(comment);
+        return comment;
     }
 
     @Transactional(transactionManager = "h2TxManager", readOnly = true)
@@ -78,13 +82,11 @@ public class CommentService {
 
     //미사용 상태 변경
     public boolean changeStateComment(Long commentId, String password) {
-
         if (commentId == null) {
             throw new IllegalArgumentException("댓글 아이디 미입력");
         }
 
         Comment comment = commentRepository.findComment(commentId);
-
         if (comment != null) {
             if (comment.getPassword().equals(password)) {
                 commentRepository.changeStateComment(comment);

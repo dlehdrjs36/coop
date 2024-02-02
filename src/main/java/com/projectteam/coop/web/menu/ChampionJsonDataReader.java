1b9046c74d46fb012f43e5c5f265d585fcecd596
dglee <slsldkf123@naver.com>
2024-02-02 21:22:03 +0900

    [리팩토링] 리소스 읽기 수정

--- src/main/java/com/projectteam/coop/web/menu/ChampionJsonDataReader.java
+++ src/main/java/com/projectteam/coop/web/menu/ChampionJsonDataReader.java
@@ -2,28 +2,30 @@ package com.projectteam.coop.web.menu;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
+@Slf4j
 @Configuration
 public class ChampionJsonDataReader {
 
-    private final ResourceLoader resourceLoader = new DefaultResourceLoader();
-    private final Resource resource = resourceLoader.getResource("classpath:/static/json/tft/");
     private final ObjectMapper mapper = new ObjectMapper();
     private JsonNode championListJsonObject = null;
 
-    public JsonNode setChampionListJsonObject(){
+    public JsonNode setChampionListJsonObject() {
         // JSON 파일 읽기
-        try {
-            championListJsonObject = mapper.readTree(new File(resource.getURL().getPath()+"championList.json"));
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("static/json/tft/championList.json")) {
+            championListJsonObject = mapper.readTree(is);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("{errorMsg}", e);
         }
         return championListJsonObject;
     }
